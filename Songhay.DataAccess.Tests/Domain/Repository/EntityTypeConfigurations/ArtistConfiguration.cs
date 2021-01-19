3490729d3992c073929cdc200bfaaaa9f2c58fed
Bryan Wilhite <rasx@songhaysystem.com>
2021-01-18 16:06:54 -0800

    #8 ❄

--- Songhay.DataAccess.Tests/Domain/Repository/EntityTypeConfigurations/ArtistConfiguration.cs
+++ Songhay.DataAccess.Tests/Domain/Repository/EntityTypeConfigurations/ArtistConfiguration.cs
@@ -16,9 +16,9 @@ namespace Songhay.DataAccess.Tests.Domain.Repository.EntityTypeConfigurations
                 .HasMaxLength(120);
 
             // Table & Column Mappings
-            builder.ToTable("Artist");
-            builder.Property(t => t.ArtistId).HasColumnName("ArtistId");
-            builder.Property(t => t.Name).HasColumnName("Name");
+            builder.ToTable(nameof(Artist));
+            builder.Property(t => t.ArtistId).HasColumnName(nameof(Artist.ArtistId));
+            builder.Property(t => t.Name).HasColumnName(nameof(Artist.Name));
         }
     }
 }
