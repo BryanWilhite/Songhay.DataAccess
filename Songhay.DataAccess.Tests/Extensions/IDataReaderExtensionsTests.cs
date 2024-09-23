66978ca06bef9418b03855243d7c7c2b6d0ef2a4
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-09-22 23:23:50 -0700

    add `IDataReaderExtensionsTests`  🔬✨

--- Songhay.DataAccess.Tests/Extensions/IDataReaderExtensionsTests.cs
+++ Songhay.DataAccess.Tests/Extensions/IDataReaderExtensionsTests.cs
@@ -1,12 +1,13 @@
 using System.Data;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Xml.Linq;
 using Microsoft.Data.Sqlite;
 using Songhay.DataAccess.Extensions;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Songhay.DataAccess.Tests;
+namespace Songhay.DataAccess.Tests.Extensions;
 
 // ReSharper disable once InconsistentNaming
 public class IDataReaderExtensionsTests
@@ -35,6 +36,27 @@ public class IDataReaderExtensionsTests
         Assert.NotNull(actual);
 
         _helper.WriteLine(actual.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    [Theory]
+    [InlineData("../../../../db/northwind.db", "SELECT * FROM Employees")]
+    public void ToXDocument_Test(string dbPath, string sql)
+    {
+        dbPath = ProgramAssemblyUtility.GetPathFromAssembly(GetType().Assembly, dbPath);
+        Assert.True(File.Exists(dbPath));
+
+        string connectionString = $"data source={dbPath}";
+
+        using IDbConnection connection = CommonDbmsUtility.GetConnection(SqliteFactory.Instance, connectionString);
+        Assert.NotNull(connection);
+        connection.Open();
+
+        using IDataReader reader = CommonReaderUtility.GetReader(connection, sql);
+
+        XDocument actual = reader.ToXDocument();
+        Assert.NotNull(actual);
+
+        _helper.WriteLine(actual.ToString());
     }
 
     readonly ITestOutputHelper _helper;
